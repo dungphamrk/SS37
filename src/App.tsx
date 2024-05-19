9784d104867a74482349af18af75915520905a49
dungphamrk <147359437+dungphamrk@users.noreply.github.com>
2024-05-20 00:10:39 +0700

    SS37

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import BT2 from './components/BT2'
+import BT5 from './components/BT5'
+import BT6 from './components/BT6'
+import BT7 from './components/BT7'
+import BT8 from './components/BT8'
+import BT9 from './components/BT9-10'
+
+export default function App() {
+  return (
+    <div>
+      <BT2/>
+      <BT5/>
+      <BT6/>
+      <BT7/>
+      <BT8/>
+      <BT9/>
+    </div>
+  )
+}
